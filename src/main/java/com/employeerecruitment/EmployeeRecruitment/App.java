2197c3f3e9de4b26be5b3148bbcfb35df8263cb0
nehatambe <nehatambe1191@gmail.com>
2018-01-20 00:47:24 -0800

    first commit

---
+++ src/main/java/com/employeerecruitment/EmployeeRecruitment/App.java
@@ -0,0 +1,26 @@
+package com.employeerecruitment.EmployeeRecruitment;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+    public int add(int a, int b) {
+		return a + b;
+	}
+	public int sub(int a, int b) {
+		return a - b;
+	}
+	public int mul(int a, int b) {
+		return a * b;
+	}
+	public int div(int a, int b) {
+		return a / b;
+	}
+	
+}
